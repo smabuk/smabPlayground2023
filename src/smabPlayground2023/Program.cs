f06fe2b2acbc16a7090ad035e6c4e33e2183bcd0
smabuk <2011834+smabuk@users.noreply.github.com>
2023-07-30 14:16:06 +0100

    Stricter .editorconfig

--- src/smabPlayground2023/Program.cs
+++ src/smabPlayground2023/Program.cs
@@ -1,4 +1,4 @@
-using smabPlayground2023;
+﻿using smabPlayground2023;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,9 +10,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    _ = app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    _ = app.UseHsts();
 }
 
 app.UseHttpsRedirection();
