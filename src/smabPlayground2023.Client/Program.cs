635ac7e25680a62171ccef40b5b885e1b43f1330
smabuk <2011834+smabuk@users.noreply.github.com>
2023-08-06 14:12:17 +0100

    Add WebAssemb;ly client project and remove a lot of the innards

---
+++ src/smabPlayground2023.Client/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+using smabPlayground2023.Client;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
