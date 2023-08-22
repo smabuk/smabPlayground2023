093d7fd522d280aee20a46b65b9b1a7cb5a8f68f
smabuk <2011834+smabuk@users.noreply.github.com>
2023-08-22 11:51:42 +0100

    OneDriveTests - note 2 of these fail under .NET Preview 7. They are expcted to work when RC1 releases

---
+++ tests/smabPlayground2023.Tests/Webtests/OneDriveTests.cs
@@ -0,0 +1,21 @@
+﻿namespace smabPlayground2023.Tests.Webtests;
+public class OneDriveTests : WebTestBase
+{
+	[Theory]
+	[InlineData("0", "Lorem ipsum dolor sit amet")]
+	[InlineData("1", "Peugeot 306 Xsi")]
+	[InlineData("2", "   1  Sat 26-Dec-2009  2009  Cloudy with a Chance of Meatballs")]
+	[InlineData("3", "Error: Invalid file index")]
+	[InlineData("-1", "Error: Invalid file index")]
+	public async Task OneDrive_File_Contains_Expected_String(string fileNumber, string expectedString)
+	{
+		HttpClient client = Factory.CreateClient();
+		using HttpResponseMessage response = await client.GetAsync($"onedrivefiles01/{fileNumber}");
+		response.IsSuccessStatusCode.ShouldBe(true);
+
+		string html = await response.Content.ReadAsStringAsync();
+		IDocument document = await AngleSharp.OpenAsync(req => req.Content(html));
+		document.Body?.InnerHtml.Contains(expectedString).ShouldBeTrue();
+	}
+
+}
