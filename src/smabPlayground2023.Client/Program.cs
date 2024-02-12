416b7cbc1313a8970557314429195de6c674a9a5
smabuk <2011834+smabuk@users.noreply.github.com>
2024-02-12 15:20:12 +0000

    v1.0.44 ClientOnly components and properties

--- src/smabPlayground2023.Client/Program.cs
+++ src/smabPlayground2023.Client/Program.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+﻿using System.Globalization;
+using System.Reflection;
+
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
@@ -18,3 +21,35 @@ Smab.DiceAndTiles.DictionaryOfWords dictionaryOfWords = words is [] ? new() : ne
 _ = builder.Services.AddSingleton(dictionaryOfWords);
 
 await builder.Build().RunAsync();
+
+
+public partial class ClientProgram
+{
+	private static readonly string[] cultures = CultureInfo
+		.GetCultures(CultureTypes.AllCultures)
+		.Select(c => c.Name)
+		.ToArray();
+
+	public static string SiteName { get; set; } = "smabPlayground2023";
+
+	public static string Name { get; } = typeof(Program)
+		.Assembly
+		.GetName()
+		.Name ?? "No assembly name";
+	public static Version Version { get; } = typeof(Program)
+		.Assembly
+		.GetName()
+		.Version ?? new();
+	public static string ProductVersion { get; } =
+		VersionWithoutGuid(typeof(Program)
+		.Assembly
+		.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+		.InformationalVersion ?? "");
+	public static string FrameworkVersion { get; } = System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription;
+
+	private static string VersionWithoutGuid(string version)
+	{
+		int indexOfPlus = version.IndexOf('+');
+		return (indexOfPlus > 0) ? version[..indexOfPlus] : version;
+	}
+} // also required so you can reference it from tests
