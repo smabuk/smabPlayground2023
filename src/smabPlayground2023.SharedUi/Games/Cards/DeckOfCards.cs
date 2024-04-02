f043cfa5b92ba822aec2f8f609332cb198c81900
smabuk <2011834+smabuk@users.noreply.github.com>
2024-04-02 21:58:27 +0100

    DeckOfCards v2 WIP

---
+++ src/smabPlayground2023.SharedUi/Games/Cards/DeckOfCards.cs
@@ -0,0 +1,26 @@
+﻿namespace smabPlayground2023.SharedUi.Games.Cards;
+public class DeckOfCards(DeckType deckType)
+{
+	public DeckType DeckType { get; set; } = deckType;
+	private List<PlayingCard> deck = [];
+
+	public PlayingCard this[int index] {
+		get => deck[index];
+		set => deck[index] = value;
+	}
+
+	public DeckOfCards Shuffle()
+	{
+		PlayingCard[] deckArray = [.. deck];
+		Random.Shared.Shuffle(deckArray);
+		deck = [..deckArray];
+		return this;
+	}
+}
+
+public enum DeckType
+{
+	DealFromTheTop,
+	DealFromTheBottom,
+	Shoe = DealFromTheBottom,
+}
