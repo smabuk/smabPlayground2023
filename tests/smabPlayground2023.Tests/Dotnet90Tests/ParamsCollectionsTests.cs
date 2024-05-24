521cf65b50efb48d1d37e09cb8412ebb5cb4b99d
smabuk <2011834+smabuk@users.noreply.github.com>
2024-05-24 23:16:54 +0100

    C# 13 features - Params collections can be something other than arrays

---
+++ tests/smabPlayground2023.Tests/Dotnet90Tests/ParamsCollectionsTests.cs
@@ -0,0 +1,14 @@
+﻿namespace smabPlayground2023.Tests.Dotnet90Tests;
+
+public class ParamsCollectionsTests
+{
+	[Fact]
+	public void ParamsCollections_CanBe_NotArrays()
+	{
+		GetSpanInts(1, 2, 3).ShouldBe([1, 2, 3]);
+		GetIEnumerableInts(1, 2, 3).ShouldBe([1, 2, 3]);
+
+		int[] GetSpanInts(params ReadOnlySpan<int> ints) => ints.ToArray();
+		int[] GetIEnumerableInts(params IEnumerable<int> ints) => ints.ToArray();
+	}
+}
