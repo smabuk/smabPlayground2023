51df3d0ac54b6716fe5c457375a5237d3558f70e
smabuk <2011834+smabuk@users.noreply.github.com>
2024-05-25 10:56:19 +0100

    C# 13 features - Escape sequence \e matches escape

---
+++ tests/smabPlayground2023.Tests/Dotnet90Tests/EscapeSequenceTests.cs
@@ -0,0 +1,12 @@
+﻿namespace smabPlayground2023.Tests.Dotnet90Tests;
+
+public class EscapeSequenceTests
+{
+	[Theory]
+	[InlineData('\u001b')]
+	[InlineData(27)]
+	public void Escape_EscapeSequemce_ShouldBe(char c)
+	{
+		'\e'.ShouldBe(c);
+	}
+}
