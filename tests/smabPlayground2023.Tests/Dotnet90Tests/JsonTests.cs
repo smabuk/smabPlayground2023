d3caed6d25bb38c773de2060ce26821a1c82e688
smabuk <2011834+smabuk@users.noreply.github.com>
2024-05-25 11:41:49 +0100

    Json Library in 9.0 - JsonSerializerOptions

---
+++ tests/smabPlayground2023.Tests/Dotnet90Tests/JsonTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+
+namespace smabPlayground2023.Tests.Dotnet90Tests;
+
+public class JsonTests
+{
+	[Fact]
+	public void Json_ShouldSerializeWith_IndentOptions()
+	{
+		JsonSerializerOptions options = new()
+		{
+			WriteIndented = true,
+			IndentCharacter = ' ',
+			IndentSize = 6,
+			PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+		};
+
+		JsonSerializer.Serialize(new { String = "something", Number = 10 }, options)
+			.ShouldBe("""
+				{
+				      "string": "something",
+				      "number": 10
+				}
+				""");
+	}
+
+	[Fact]
+	public void Json_ShouldSerializeWith_WebDefaults()
+	{
+		JsonSerializer.Serialize(new { String = "something", Number = 10 }, JsonSerializerOptions.Web)
+			.ShouldBe("""{"string":"something","number":10}""");
+	}
+}
