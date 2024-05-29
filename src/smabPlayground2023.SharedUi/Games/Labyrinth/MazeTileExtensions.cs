db432503523bdf1172c060ba7b44c67a3c538f47
smabuk <2011834+smabuk@users.noreply.github.com>
2024-05-29 21:23:05 +0100

    Labyrinth - Pushing faded tiles

--- src/smabPlayground2023.SharedUi/Games/Labyrinth/MazeTileExtensions.cs
+++ src/smabPlayground2023.SharedUi/Games/Labyrinth/MazeTileExtensions.cs
@@ -5,54 +5,54 @@ public static class MazeTileExtensions
 
 	public static MazeTile Rotate(this MazeTile tile, int amount) => tile with { Orientation = (tile.Orientation + amount) % 360 };
 
-	public static bool HasNorthExit(this MazeTile card) => card.Orientation switch
+	public static bool HasNorthExit(this MazeTile tile) => tile.Orientation switch
 	{
-		0 => card.NorthExit,
-		90 => card.WestExit,
-		180 => card.SouthExit,
-		270 => card.EastExit,
+		0 => tile.NorthExit,
+		90 => tile.WestExit,
+		180 => tile.SouthExit,
+		270 => tile.EastExit,
 		_ => false,
 	};
 
-	public static bool HasEastExit(this MazeTile card) => card.Orientation switch
+	public static bool HasEastExit(this MazeTile tile) => tile.Orientation switch
 	{
-		0 => card.EastExit,
-		90 => card.NorthExit,
-		180 => card.WestExit,
-		270 => card.SouthExit,
+		0 => tile.EastExit,
+		90 => tile.NorthExit,
+		180 => tile.WestExit,
+		270 => tile.SouthExit,
 		_ => false,
 	};
 
-	public static bool HasSouthExit(this MazeTile card) => card.Orientation switch
+	public static bool HasSouthExit(this MazeTile tile) => tile.Orientation switch
 	{
-		0 => card.SouthExit,
-		90 => card.EastExit,
-		180 => card.WestExit,
-		270 => card.NorthExit,
+		0 => tile.SouthExit,
+		90 => tile.EastExit,
+		180 => tile.WestExit,
+		270 => tile.NorthExit,
 		_ => false,
 	};
 
-	public static bool HasWestExit(this MazeTile card) => card.Orientation switch
+	public static bool HasWestExit(this MazeTile tile) => tile.Orientation switch
 	{
-		0 => card.WestExit,
-		90 => card.SouthExit,
-		180 => card.EastExit,
-		270 => card.NorthExit,
+		0 => tile.WestExit,
+		90 => tile.SouthExit,
+		180 => tile.EastExit,
+		270 => tile.NorthExit,
 		_ => false,
 	};
 
-	public static bool HasExit(this MazeTile card, ReadOnlySpan<char> exit)
+	public static bool HasExit(this MazeTile tile, ReadOnlySpan<char> exit)
 	{
 		return exit[0] switch
 		{
-			'N' or 'n' => card.HasNorthExit(),
-			'E' or 'e' => card.HasEastExit(),
-			'S' or 's' => card.HasSouthExit(),
-			'W' or 'w' => card.HasWestExit(),
+			'N' or 'n' => tile.HasNorthExit(),
+			'E' or 'e' => tile.HasEastExit(),
+			'S' or 's' => tile.HasSouthExit(),
+			'W' or 'w' => tile.HasWestExit(),
 			_ => throw new ArgumentOutOfRangeException(nameof(exit)),
 		};
 	}
 
-	public static string PathDirections(this MazeTile card)
-		=> $"{(card.HasNorthExit() ? "N" : "")}{(card.HasEastExit() ? "E" : "")}{(card.HasSouthExit() ? "S" : "")}{(card.HasWestExit() ? "W" : "")}";
+	public static string PathDirections(this MazeTile tile)
+		=> $"{(tile.HasNorthExit() ? "N" : "")}{(tile.HasEastExit() ? "E" : "")}{(tile.HasSouthExit() ? "S" : "")}{(tile.HasWestExit() ? "W" : "")}";
 }
