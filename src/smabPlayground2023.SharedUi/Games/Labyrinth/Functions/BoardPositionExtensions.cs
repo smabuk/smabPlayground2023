f9f8130533f0083840877ea20a708d51a0ee4cdb
smabuk <2011834+smabuk@users.noreply.github.com>
2024-06-01 18:05:03 +0100

    Labyrinth - Drag and Drop

--- src/smabPlayground2023.SharedUi/Games/Labyrinth/Functions/BoardPositionExtensions.cs
+++ src/smabPlayground2023.SharedUi/Games/Labyrinth/Functions/BoardPositionExtensions.cs
@@ -3,4 +3,7 @@
 public static class BoardPositionExtensions
 {
 	public static bool IsAt(this BoardPosition boardPosition, int col, int row) => boardPosition.Col == col && boardPosition.Row == row;
+
+	public static bool IsAtTheSamePosition(this BoardPosition position, BoardPosition position2)
+		=> position.Col == position2.Col && position.Row == position2.Row;
 }
