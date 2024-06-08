cae58da208ac0d009c19a63ba01ee89ff6ac921d
smabuk <2011834+smabuk@users.noreply.github.com>
2024-06-08 13:56:35 +0100

    1.1.18 Fix for DiceAndTiles updates

---
+++ src/smabPlayground2023.SharedUi/Games/Labyrinth/Models/PushResult.cs
@@ -0,0 +1,17 @@
+﻿namespace smabPlayground2023.SharedUi.Games.Labyrinth.Models;
+
+public record PushResult(string Word, int Score, PushResultReason Reason)
+{
+	public static implicit operator (string word, int score, PushResultReason reason)(PushResult value)
+		=> (value.Word, value.Score, value.Reason);
+
+	public static implicit operator PushResult((string word, int score, PushResultReason reason) value)
+		=> new(value.word, value.score, value.reason);
+}
+
+public enum PushResultReason
+{
+	Success = 0,
+	InvalidColumnOrRow,
+	Blocked,
+}
