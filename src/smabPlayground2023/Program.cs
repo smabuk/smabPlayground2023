d1cbf5d02c7d47652e9b54be509331f27022356e
smabuk <2011834+smabuk@users.noreply.github.com>
2024-06-12 11:22:41 +0100

    1.1.24 Upgrade to .NET 9.0 Preview 5

--- src/smabPlayground2023/Program.cs
+++ src/smabPlayground2023/Program.cs
@@ -39,9 +39,9 @@ if (app.Environment.IsDevelopment()) {
 
 app.UseHttpsRedirection();
 
-app.UseStaticFiles();
-
 app.UseAntiforgery();
+
+app.MapStaticAssets();
 
 app.MapRazorComponents<App>()
 	.AddInteractiveServerRenderMode()
