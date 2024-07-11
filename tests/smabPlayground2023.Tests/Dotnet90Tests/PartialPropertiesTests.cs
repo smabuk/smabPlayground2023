5ad4aec699832e71c84a60d2990572aac96ab2c0
smabuk <2011834+smabuk@users.noreply.github.com>
2024-07-11 11:49:18 +0100

    C# 13 features - Partial properties

---
+++ tests/smabPlayground2023.Tests/Dotnet90Tests/PartialPropertiesTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+
+namespace smabPlayground2023.Tests.Dotnet90Tests;
+public partial class PartialPropertiesTests
+{
+	[GeneratedRegex("abc|def")]
+	private static partial Regex NewMethod { get; }
+
+	[Theory]
+	[InlineData("ab", false, 0)]
+	[InlineData("abc", true, 1)]
+	[InlineData("abcdefabdefabc", true, 4)]
+	void GeneratedRegex_ShouldWorkWith_PartialProperties(string input, bool expectedMatchResult, int expectedMatchCount)
+	{
+		bool oldMatchResult = OldRegex.OldMethod().IsMatch(input);
+		bool newMatchResult = NewMethod.IsMatch(input);
+
+		oldMatchResult.ShouldBe(expectedMatchResult);
+		newMatchResult.ShouldBe(expectedMatchResult);
+
+		int oldMatchCount = OldRegex.OldMethod().Matches(input).Count();
+		int newMatchCount = NewMethod.Matches(input).Count();
+
+		oldMatchCount.ShouldBe(expectedMatchCount);
+		newMatchCount.ShouldBe(expectedMatchCount);
+	}
+
+	private static partial class OldRegex
+	{
+		[GeneratedRegex("abc|def")]
+		public static partial Regex OldMethod();
+	}
+}
+
