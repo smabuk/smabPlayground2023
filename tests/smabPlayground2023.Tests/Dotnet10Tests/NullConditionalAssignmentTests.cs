cff25e56a66aa3d50b53946cdff950493a8ed43f
smabuk <2011834+smabuk@users.noreply.github.com>
2025-04-10 23:04:02 +0100

    1.1.40 C#14 feature Null-conditional assignment

---
+++ tests/smabPlayground2023.Tests/Dotnet10Tests/NullConditionalAssignmentTests.cs
@@ -0,0 +1,30 @@
+﻿namespace smabPlayground2023.Tests.Dotnet10Tests;
+
+public class NullConditionalAssignmentTests
+{
+	private class Customer
+	{
+		public string? Name { get; set; }
+		public int Age { get; set; }
+	}
+
+	[Fact]
+	public void Assignment_ShouldSucceed()
+	{
+		Customer? customer = new()
+		{
+			Name = "Not null"
+		};
+
+		customer?.Name = "Default Name";
+		customer?.Name.ShouldBe("Default Name");
+	}
+
+	[Fact]
+	public void Assignment_ShouldFail()
+	{
+		Customer? customer = null;
+		customer?.Name = "Default Name";
+		customer?.Name.ShouldBeNull();
+	}
+}
